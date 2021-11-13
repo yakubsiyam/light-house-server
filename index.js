2a97328766a8bc76171ed22ea3755d86a3b0941c
Siyam <itsyakubsiyam@gmail.com>
2021-11-13 15:33:19 +0600

    cart filtering

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
@@ -8,8 +9,10 @@ const ObjectId = require("mongodb").ObjectId;
 
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cgvvx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -41,11 +44,11 @@ async function run() {
     });
 
     // found my carts
-    app.get("/carts", async (req, res) => {
-      const cursor = cartsCollection.find({});
-      const carts = await cursor.toArray();
-      res.send(carts);
-    });
+    // app.get("/carts", async (req, res) => {
+    //   const cursor = cartsCollection.find({});
+    //   const carts = await cursor.toArray();
+    //   res.send(carts);
+    // });
 
     // add mycart
     app.post("/carts", async (req, res) => {
@@ -56,18 +59,34 @@ async function run() {
 
     // my cart data filtering
     app.get("/carts", async (req, res) => {
-      // let query = {};
-      // const email = req.query.email;
-      // if (email) {
-      //   query = { email: email };
-      // }
+      let query = {};
       const email = req.query.email;
-      console.log(email);
-      const query = { email: email };
-      console.log(query);
+      if (email) {
+        query = { email: email };
+      }
+
       const cursor = cartsCollection.find(query);
       const myCarts = await cursor.toArray();
       res.send(myCarts);
+    });
+
+    //update status data
+    app.put("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateStatus = "accepted";
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updateStatus,
+        },
+      };
+      const updatedLamps = await cartsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(updatedLamps);
     });
 
     // delete my cart lamps
